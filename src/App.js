cdea9bd0e238f55fdb0d019bff25bf262d0215e0
BenBezac <benjamin.bezancon@gmail.com>
2020-02-07 10:28:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {items: [], isLoading: true};
+    }
+
+    componentDidMount() {
+        const API_URL = process.env.REACT_APP_API_URL;
+
+        const url = API_URL + '/shopperElements';
+        fetch(url)
+            .then(response => response.json())
+            .then(data => this.setState({items: data._embedded.shopperElements, isLoading: false}));
+    }
+
+    render() {
+        console.log(process.env)
+        console.log(this.state.items)
+        return (
+            <div>
+                {this.state.items.map( (shopperElement, index) =>
+                    <div key={index} className="row">
+                        <div className="col-12">
+                            <label>Nom: </label> {shopperElement.name}
+                        </div>
+                        <div className="col-12">
+                            <label>Description: </label> {shopperElement.description}
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
